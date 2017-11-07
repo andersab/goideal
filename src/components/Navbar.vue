feb187bf7484d16e1a83dd2a5619dfa69e895b85
Brad Anderson <anderson.ab@gmail.com>
2017-11-06 22:43:31 -0500

    updates

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<<template>
+  <div class="navbar">
+      <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/ideas">Ideas</a></li>
+          <li><a href="/mindmaps">Mindmaps</a></li>
+      </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'navbar'
+}
+</script>
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: silver;
+}
+li {
+    display: inline;
+    border-right: 1px solid #bbbbbb;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    float: left;
+}
+
+a:link {
+    color:maroon;
+}
+
+li:last-child {
+    border-right: none;
+}
+
+.active {
+    background-color: maroon;
+    color: silver;
+}
+</style>
